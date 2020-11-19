9dbda766a49bb911b7c8ac1b86d14454ae8b88a2
t-xinlin <t_xinlin@sina.com>
2020-11-19 21:19:52 +0800

    Create main.rs

---
+++ iterator/main.rs
@@ -0,0 +1,29 @@
+#[derive(PartialEq, Debug)]
+struct Shoe {
+    size: u32,
+    style: String,
+}
+fn shoes_in_my_size(shoes: Vec<Shoe>, shoe_size: u32) -> Vec<Shoe> {
+    shoes.into_iter().filter(|s| s.size == shoe_size).collect()
+}
+
+fn main() {
+    let shoes = vec![
+        Shoe {
+            size: 10,
+            style: String::from("sneaker"),
+        },
+        Shoe {
+            size: 13,
+            style: String::from("sandal"),
+        },
+        Shoe {
+            size: 10,
+            style: String::from("boot"),
+        },
+    ];
+
+    let in_my_size = shoes_in_my_size(shoes, 10);
+
+    println!("mysize: {:?}", in_my_size)
+}
