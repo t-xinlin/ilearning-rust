333e8a64e28c21ac939ecdee485f090ead8c0b7c
t-xinlin <t_xinlin@sina.com>
2021-05-29 18:52:15 +0800

    Create read_request_body.rs

---
+++ actix-web-example/src/middleware/read_request_body.rs
@@ -0,0 +1,71 @@
+use std::cell::RefCell;
+use std::pin::Pin;
+use std::rc::Rc;
+use std::task::{Context, Poll};
+
+use actix_service::{Service, Transform};
+use actix_web::web::BytesMut;
+use actix_web::{dev::ServiceRequest, dev::ServiceResponse, Error, HttpMessage};
+use futures::future::{ok, Future, Ready};
+use futures::stream::StreamExt;
+use actix_web::http::StatusCode;
+use log::Level::Debug;
+
+pub struct ReadReqBody;
+
+impl<S: 'static, B> Transform<S,ServiceRequest> for ReadReqBody
+    where
+        S: Service<ServiceRequest, Response=ServiceResponse<B>, Error=Error>,
+        S::Future: 'static,
+        B: 'static,
+{
+    // type Request = ServiceRequest;
+    type Response = ServiceResponse<B>;
+    type Error = Error;
+    type Transform = ReadReqBodyMiddleware<S>;
+    type InitError = ();
+    type Future = Ready<Result<Self::Transform, Self::InitError>>;
+
+    fn new_transform(&self, service: S) -> Self::Future {
+        ok(ReadReqBodyMiddleware {
+            service: Rc::new(RefCell::new(service)),
+        })
+    }
+}
+
+pub struct ReadReqBodyMiddleware<S> {
+    service: Rc<RefCell<S>>,
+}
+
+impl<S, B> Service<ServiceRequest> for ReadReqBodyMiddleware<S>
+    where
+        S: Service<ServiceRequest, Response=ServiceResponse<B>, Error=Error> + 'static,
+        S::Future: 'static,
+        B: 'static,
+{
+    // type Request = ServiceRequest;
+    type Response = ServiceResponse<B>;
+    type Error = Error;
+    type Future = Pin<Box<dyn Future<Output=Result<Self::Response, Self::Error>>>>;
+
+    // fn poll_ready(&mut self, cx: &mut Context) -> Poll<Result<(), Self::Error>> {
+    //     self.service.poll_ready(cx)
+    // }
+    actix_service::forward_ready!(service);
+    fn call(&self, mut req: ServiceRequest) -> Self::Future {
+        let mut svc = self.service.clone();
+        Box::pin(async move {
+            let mut body = BytesMut::new();
+            let mut stream = req.take_payload();
+            while let Some(chunk) = stream.next().await {
+                body.extend_from_slice(&chunk?);
+            }
+            info!("request body: {:?}", body);
+            let res = svc.call(req).await?;
+            info!("response: {:?}", res.headers());
+            Ok(res)
+
+            // Err(Error::from(myRespError::MyError::BadClientData))
+        })
+    }
+}
