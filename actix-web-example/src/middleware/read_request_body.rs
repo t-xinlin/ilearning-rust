4bf4bd6274b98a227fdccc36badd0144ceaa9a54
t-xinlin <t_xinlin@sina.com>
2021-06-04 18:41:52 +0800

    Update read_request_body.rs

--- actix-web-example/src/middleware/read_request_body.rs
+++ actix-web-example/src/middleware/read_request_body.rs
@@ -4,16 +4,17 @@ use std::rc::Rc;
 use std::task::{Context, Poll};
 
 use actix_service::{Service, Transform};
-use actix_web::web::BytesMut;
-use actix_web::{dev::ServiceRequest, dev::ServiceResponse, Error, HttpMessage};
+use actix_web::web::{BytesMut, Payload};
+use actix_web::{error, dev::ServiceRequest, dev::ServiceResponse, Error, HttpMessage};
 use futures::future::{ok, Future, Ready};
 use futures::stream::StreamExt;
-use actix_web::http::StatusCode;
+use actix_web::http::{StatusCode};
 use log::Level::Debug;
+use crate::error::user_error;
 
 pub struct ReadReqBody;
 
-impl<S: 'static, B> Transform<S,ServiceRequest> for ReadReqBody
+impl<S: 'static, B> Transform<S, ServiceRequest> for ReadReqBody
     where
         S: Service<ServiceRequest, Response=ServiceResponse<B>, Error=Error>,
         S::Future: 'static,
@@ -52,20 +53,29 @@ impl<S, B> Service<ServiceRequest> for ReadReqBodyMiddleware<S>
     //     self.service.poll_ready(cx)
     // }
     actix_service::forward_ready!(service);
+
     fn call(&self, mut req: ServiceRequest) -> Self::Future {
+        info!("Hi from start. You requested: {}", req.path());
         let mut svc = self.service.clone();
         Box::pin(async move {
+
             let mut body = BytesMut::new();
             let mut stream = req.take_payload();
             while let Some(chunk) = stream.next().await {
                 body.extend_from_slice(&chunk?);
             }
-            info!("request body: {:?}", body);
+            info!("read body: {:?}", body);
+            // 回写body
+            let mut payload = actix_http::h1::Payload::empty();
+            payload.unread_data(body.into());
+            req.set_payload(payload.into());
+
             let res = svc.call(req).await?;
+
             info!("response: {:?}", res.headers());
             Ok(res)
-
             // Err(Error::from(myRespError::MyError::BadClientData))
         })
+
     }
 }
