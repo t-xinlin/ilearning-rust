40a061bfddfdccc0108a05fd478c267d7098b26d
t-xinlin <t_xinlin@sina.com>
2021-06-04 18:41:34 +0800

    Update jwt.rs

--- actix-web-example/src/middleware/jwt.rs
+++ actix-web-example/src/middleware/jwt.rs
@@ -2,17 +2,58 @@ use actix_web::dev::{/*Transform, Service, */ServiceRequest, ServiceResponse};
 use futures::future::{Ready, ok};
 use futures::{Future};
 use futures::stream::StreamExt;
-use actix_web::{Error, HttpMessage};
+use actix_web::{ResponseError, Error, HttpMessage, HttpRequest, HttpResponse, HttpResponseBuilder};
 use std::task::{Context, Poll};
 use std::pin::Pin;
 use std::rc::Rc;
 use std::cell::RefCell;
-use actix_web::web::BytesMut;
+use actix_web::web::{BytesMut, Buf, BufMut};
 use actix_service::{Service, Transform};
 use std::borrow::BorrowMut;
+use actix_web::error::PayloadError;
 
+use actix_http::http::{header, HeaderMap, HeaderValue, StatusCode};
+use log4rs::init_file;
+use actix_http::Protocol::Http1;
+// use actix_http::error as actixHttpError;
+use derive_more::{Display, Error};
+use actix_http::{error, body::Body, Response};
+use std::{fmt, io::Write as _};
 
 pub struct Jwt;
+
+#[derive(Debug, Display, Error)]
+pub enum UserError {
+    #[display(fmt = "Validation error on field: {}", field)]
+    ValidationError { field: String },
+}
+
+impl ResponseError for UserError {
+    fn status_code(&self) -> StatusCode {
+        match *self {
+            UserError::ValidationError { .. } => StatusCode::UNAUTHORIZED,
+        }
+    }
+    fn error_response(&self) -> Response<Body> {
+        // let mut res = Response::new(self.status_code());
+        // res.headers_mut().insert(
+        //     header::CONTENT_TYPE,
+        //     header::HeaderValue::from_static("application/json"),
+        // );
+        // res.set_body(Body::from(self.to_string())).into()
+
+
+        let mut res = Response::new(self.status_code());
+        let mut buf = BytesMut::new().writer();
+        let _ = write!(buf, "{}", self);
+
+        res.headers_mut().insert(
+            header::CONTENT_TYPE,
+            header::HeaderValue::from_static("application/json"),
+        );
+        res.set_body(Body::from(buf.into_inner())).into()
+    }
+}
 
 impl<S: 'static, B> Transform<S, ServiceRequest> for Jwt
     where S: Service<ServiceRequest, Response=ServiceResponse<B>, Error=Error>,
@@ -58,16 +99,61 @@ impl<S, B> Service<ServiceRequest> for JwtMiddleware<S>
         let mut svc = self.service.clone();
         Box::pin(async move {
             let mut body = BytesMut::new();
-            let mut stream = req.borrow_mut().take_payload();
+            let mut stream = req.take_payload();
             while let Some(chunk) = stream.next().await {
                 body.extend_from_slice(&chunk?);
             }
-            info!("jwt body: {:?}", body);
-            let res = svc.call(req).await?;
-            info!("jwt response: {:?}", res.headers());
-            Ok(res)
 
+            if let Some(sign) = get_content_type(&req, "sign".to_string()) {
+                let v: Vec<&str> = sign.split('.').collect();
+                for s in v {
+                    info!("{}", s);
+                }
+                info!("get sign = '{}'", sign);
+            } else {
+                error!("token invalid");
+                return Err(Error::from(UserError::ValidationError { field: "token invalid".to_string() }));
+            }
+
+            info!("request body: {:?}", body);
+            // 回写body
+            let mut payload = actix_http::h1::Payload::empty();
+            payload.unread_data(body.into());
+            req.set_payload(payload.into());
+
+            let res = svc.call(req).await?;
+            info!("response: {:?}", res.headers());
+            Ok(res)
             // Err(Error::from(myRespError::MyError::BadClientData))
         })
+
+        //
+        // let mut svc = self.service.clone();
+        // Box::Pin(
+        //    async move {
+        //        req.take_payload()
+        //            .fold(BytesMut::new(), move |mut body, chunk| {
+        //                let bs = chunk.unwrap().as_bytes();
+        //                body.extend_from_slice(bs);
+        //                // Ok::<_, PayloadError>(body)
+        //                Ok(())
+        //            })
+        //            .map_err(|e| Err((e.into())))
+        //            .and_then(move |bytes| {
+        //                println!("request body: {:?}", bytes);
+        //
+        //                let mut payload = actix_http::h1::Payload::empty();
+        //                payload.unread_data(bytes.into());
+        //                req.set_payload(payload.into());
+        //
+        //                svc.call(req).and_then(|res| Ok(res))
+        //            });
+        //        Ok(())
+        //    }
+        // )
     }
+}
+
+fn get_content_type(req: &ServiceRequest, key: String) -> Option<&str> {
+    req.headers().get(key)?.to_str().ok()
 }
