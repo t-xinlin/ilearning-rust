7bea3721ccf955eb5ff38e5dd9622a36ab211649
t-xinlin <t_xinlin@sina.com>
2021-08-27 18:00:29 +0800

    Update main.rs
    www

--- kafka/src/main.rs
+++ kafka/src/main.rs
@@ -4,23 +4,34 @@ use kafka_v1::async_consumer;
 use kafka_v1::async_producer;
 use kafka_v1::config;
 use kafka_v1::log::*;
+use kafka_v1::signal;
 use log::*;
-use tokio::time::Duration;
+// use tokio::signal::unix::SignalKind;
+// use tokio::sync::mpsc;
+// use tokio::time::Duration;
 
 #[tokio::main]
 async fn main() {
     init();
-    // tokio::task::spawn(simple_consumer::simple_consumer());
-    // tokio::task::spawn(async_producer::run_producer());
-    // tokio::task::spawn(async_consumer::async_consumer());
+    tokio::task::spawn(async_consumer::async_consumer());
     tokio::task::spawn(async_producer::run_producer());
-    tokio::time::sleep(Duration::from_secs(60 * 10)).await;
-    info!("main exit");
+
+    // tokio::time::sleep(Duration::from_secs(60 * 10)).await;
+    // let (_shutdown_tx, mut shutdown_rx) = mpsc::unbounded_channel();
+    tokio::select! {
+        _ = signal::shutdown() => {
+            info!("Received shutdown signal");
+        }
+        // _ = shutdown_rx.recv() => {
+        //     info!("Received shutdown via admin interface");
+        // }
+    }
+    info!("service shutdown")
 }
 
 fn init() {
     log_init();
-    config::init_config();
+    // config::init_config();
     let k = config::init_kafka();
     info!("kafka config: {:?}", k)
 }
