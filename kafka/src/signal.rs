5db6262e576c86b62185672eb6f41bf9b13de1bc
t-xinlin <t_xinlin@sina.com>
2021-08-27 18:00:03 +0800

    Create signal.rs

---
+++ kafka/src/signal.rs
@@ -0,0 +1,60 @@
+//! Unix signal handling for the proxy binary.
+
+// #![deny(warnings, rust_2018_idioms)]
+// #![forbid(unsafe_code)]
+
+/// Returns a `Future` that completes when the proxy should start to shutdown.
+pub async fn shutdown() {
+    imp::shutdown().await
+}
+
+#[cfg(unix)]
+mod imp {
+    use tokio::signal::unix::{signal, SignalKind};
+    use log::*;
+
+    pub(super) async fn shutdown() {
+        tokio::select! {
+            // SIGINT  - To allow Ctrl-c to emulate SIGTERM while developing.
+            () = sig(SignalKind::interrupt(), "SIGINT") => {}
+            // SIGTERM - Kubernetes sends this to start a graceful shutdown.
+            () = sig(SignalKind::terminate(), "SIGTERM") => {}
+        };
+    }
+
+    async fn sig(kind: SignalKind, name: &'static str) {
+        // Create a Future that completes the first
+        // time the process receives 'sig'.
+        signal(kind)
+            .expect("Failed to register signal handler")
+            .recv()
+            .await;
+        info!(
+            // use target to remove 'imp' from output
+            target: "linkerd_proxy::signal",
+            "received {}, starting shutdown",
+            name,
+        );
+    }
+}
+
+#[cfg(not(unix))]
+mod imp {
+    use log::*;
+
+    pub(super) async fn shutdown() {
+        // On Windows, we don't have all the signals, but Windows also
+        // isn't our expected deployment target. This implementation allows
+        // developers on Windows to simulate proxy graceful shutdown
+        // by pressing Ctrl-C.
+        tokio::signal::windows::ctrl_c()
+            .expect("Failed to register signal handler")
+            .recv()
+            .await;
+        info!(
+            // use target to remove 'imp' from output
+            target: "linkerd_proxy::signal",
+            "received Ctrl-C, starting shutdown",
+        );
+    }
+}
