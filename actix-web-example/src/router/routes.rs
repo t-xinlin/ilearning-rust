e1fa2c48533d82b4395681402520ae20026a430b
t-xinlin <t_xinlin@sina.com>
2022-04-11 19:29:05 +0800

    Create routes.rs

---
+++ actix-web-example/src/router/routes.rs
@@ -0,0 +1,29 @@
+//! router setting
+use actix_web::web;
+use crate::{
+    middleware,
+    handler::{basic, user},
+};
+
+pub fn routes(cfg: &mut web::ServiceConfig) {
+    cfg
+        // .wrap(middleware::ReadReqBody)
+        // .wrap(middleware::Jwt)
+        // // .wrap(middleware::AccessLogging)
+        // .wrap(middleware::AccessLogging::default().log_target("http_log"))
+        // .app_data(counter.clone()) // <- register the created data
+        .service(basic::index)
+
+        .service(
+            // /app
+            web::scope("/app")
+                .wrap(middleware::Jwt)
+                // .route("/user", web::post().to(user::user_handler))
+                .route("/greet", web::get().to(basic::greet))
+                .route("/state", web::post().to(basic::state))
+                .service(user::bytes_handler)
+                .service(user::extract_json_handler)
+                .service(user::json_handler)
+                .service(user::payload_handler),
+        );
+}
