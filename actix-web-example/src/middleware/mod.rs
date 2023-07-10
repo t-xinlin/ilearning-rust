d317afc69c4542e921910101c45e219aadfcfce3
t-xinlin <t_xinlin@sina.com>
2023-07-10 19:41:51 +0800

    Update mod.rs

--- actix-web-example/src/middleware/mod.rs
+++ actix-web-example/src/middleware/mod.rs
@@ -1,2 +1,9 @@
-pub mod jwt;
-pub mod read_request_body;
+mod jwt;
+mod read_request_body;
+mod read_response_body;
+mod access_log;
+mod logger;
+
+pub use self::jwt::Jwt;
+pub use self::read_request_body::ReadReqBody;
+pub use self::access_log::AccessLogging;
