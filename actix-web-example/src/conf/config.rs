32d9173e09351b107fbb2dacb945128f2c5b9315
t-xinlin <t_xinlin@sina.com>
2023-08-10 19:41:57 +0800

    Update config.rs

--- actix-web-example/src/conf/config.rs
+++ actix-web-example/src/conf/config.rs
@@ -4,46 +4,121 @@ use std::io::prelude::*;
 use std::path::Path;
 use std::sync::Arc;
 use std::sync::Mutex;
+// use lazy_static::lazy::Lazy;
 
 use serde::Deserialize;
 use toml;
-
 use validator::{Validate, ValidationError};
+use once_cell::sync::Lazy;
+// use sqlite::Connection;
+use sqlx::{Connection, Executor, Pool, Sqlite, sqlite::SqlitePoolOptions, SqliteConnection};
+use sqlx::migrate::MigrateDatabase;
 
 lazy_static::lazy_static! {
-    pub static ref CACHE:  Arc<Mutex<Conf>> =    Arc::new(Mutex::new(Conf::new().unwrap()));
+    pub static ref GLOBAL_CONFIG: Arc<Mutex<Conf>> = Arc::new(Mutex::new(Conf::new().unwrap()));
+    // pub static ref CONN: Arc<Mutex<Pool<Sqlite>>> = Arc::new(Mutex::new(setup_db("actix-web-example.db").unwrap()));
+    // pub static ref SQLITE_CONN: Arc<Mutex<Pool<Sqlite>>> = Arc::new(Mutex::new(setup_db("actix-web-example.db").unwrap()));
 }
 
-fn validate_username(username: &str) -> Result<(), ValidationError> {
-    if username == "xXxShad0wxXx" {
+pub static SQLITE_CONN: Lazy<Pool<Sqlite>> = Lazy::new(|| setup_db("actix-web-example.db").unwrap());
+
+// lazy_static::lazy_static! {
+//     pub static ref CONN: Arc<Mutex<Connection>> = Arc::new(Mutex::new(setup_users("actix-web-example.db")));
+// }
+// lazy_static::lazy_static! {
+//     pub static ref CACHE:Arc<Mutex<Connection>> = Arc::new(Mutex::new(Conf::new().unwrap()));
+// }
+
+
+pub async fn init_db() {
+    Sqlite::create_database("actix-web-example.db").await.expect("init db error");
+    let mut conn = SqliteConnection::connect("actix-web-example.db").await.unwrap();
+    let query = "CREATE TABLE IF NOT EXISTS courses (\
+            id TEXT, \
+            teacher_id INTEGER, \
+            name TEXT, \
+            time TEXT, \
+            description TEXT, \
+            format TEXT, \
+            structure TEXT, \
+            duration TEXT, \
+            price DOUBLE, \
+            language TEXT, \
+            level TEXT);";
+    conn.execute(query).await.unwrap();
+}
+
+pub fn setup_db<T: AsRef<Path>>(_path: T) -> Result<Pool<Sqlite>, sqlx::Error> {
+    debug!("init db");
+    let pool = SqlitePoolOptions::new()
+        .max_connections(100)
+        .min_connections(10)
+        // .connect_timeout(std::time::Duration::from_secs(10))
+        .max_lifetime(std::time::Duration::from_secs(1800))
+        .idle_timeout(std::time::Duration::from_secs(600))
+        // .connect("actix-web-example.db"). await?.unwrap();
+        .connect_lazy("actix-web-example.db").unwrap();
+    Ok(pool)
+}
+
+#[derive(Deserialize, Debug, Validate, Clone)]
+pub struct Package {
+    pub name: Option<String>,
+    pub version: Option<String>,
+    pub authors: Option<Vec<String>>,
+}
+
+#[derive(Deserialize, Debug, Validate, Clone)]
+pub struct Server {
+    pub name: Option<String>,
+    pub address: Option<String>,
+    pub port: Option<i64>,
+    pub services: Option<Vec<Address>>,
+}
+
+#[derive(Deserialize, Debug, Validate, Clone)]
+pub struct Address {
+    pub address: Option<String>,
+    #[validate(custom(function = "validate_port", message = "invalid port"))]
+    pub port: Option<i64>,
+}
+
+#[derive(Deserialize, Debug, Validate, Clone)]
+pub struct Log {
+    file: Option<String>,
+}
+
+#[derive(Deserialize, Debug, Validate, Clone)]
+pub struct Database {
+    pub db_type: Option<String>,
+    pub host: Option<String>,
+    pub port: Option<i64>,
+    pub name: Option<String>,
+    pub user: Option<String>,
+    pub password: Option<String>,
+    pub ssl_enable: Option<bool>,
+    pub max_idle: Option<String>,
+    pub max_open: Option<String>,
+}
+
+#[derive(Deserialize, Debug, Validate)]
+pub struct Conf {
+    #[validate]
+    pub package: Package,
+    #[validate]
+    pub log: Log,
+    #[validate]
+    pub server: Server,
+
+}
+
+fn validate_port(p: i64) -> Result<(), ValidationError> {
+    if p == 0 {
         // the value of the username will automatically be added later
         // return Err(ValidationError::new("invalid name"));
         debug!("invalid name")
     }
     Ok(())
-}
-
-#[derive(Deserialize, Debug, Validate, Clone)]
-pub struct Server {
-    address: Option<String>,
-    port: Option<i64>,
-}
-
-#[derive(Deserialize, Debug, Validate, Clone)]
-pub struct Log {
-    #[validate(custom(function = "validate_username", message = "invalid name"))]
-    username: String,
-    file: Option<String>,
-}
-
-#[derive(Deserialize, Debug, Validate, Clone)]
-pub struct Conf {
-    #[validate]
-    pub server: Server,
-    #[validate]
-    pub log: Log,
-    // #[validate]
-    // pub ip_config: Option<Vec<IpConfig>>,
 }
 
 impl Conf {
@@ -65,10 +140,20 @@ impl Conf {
         // validate config
         config.validate().unwrap();
         let s = config.clone();
-        debug!("config.server.address:{:?}", s.server.address);
-        debug!("config.server.port:{:?}", s.server.port);
-        debug!("config.log.username:{:?}", s.log.username);
+        debug!("config.package.name:{:?}", s.package.name);
+        debug!("config.package.version:{:?}", s.package.version);
+        debug!("config.package.authors:{:?}", s.package.authors);
         debug!("config.log.file:{:?}", s.log.file);
         Ok(config)
+    }
+}
+
+impl Clone for Conf {
+    fn clone(&self) -> Self {
+        Conf {
+            package: self.package.clone(),
+            log: self.log.clone(),
+            server: self.server.clone(),
+        }
     }
 }
