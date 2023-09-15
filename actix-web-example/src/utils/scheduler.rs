2268f7239c2ff9ea942238430b1163e9f376911b
t-xinlin <t_xinlin@sina.com>
2023-09-15 20:09:45 +0800

    Update scheduler.rs

--- actix-web-example/src/utils/scheduler.rs
+++ actix-web-example/src/utils/scheduler.rs
@@ -16,12 +16,16 @@ pub fn build() -> Result<SchedulerJob, &'static str> {
     Ok(SchedulerJob {})
 }
 
+impl SchedulerJob {
+    pub fn new() -> Result<SchedulerJob, &'static str> {
+        Ok(SchedulerJob {})
+    }
+}
+
 #[async_trait]
 impl JobTrait for SchedulerJob {
-    async fn run(&self, expression: &str) -> Result<(), JobSchedulerError> {
-        // info!("async trait run")
-
-        // let expression: &str = "1/1 * * * * *";
+    async fn run(&self, expr: &str) -> Result<(), JobSchedulerError> {
+        // let mut sched = JobScheduler::new().await?;
         let sched = JobScheduler::new().await?;
 
         // sched.shutdown_on_ctrl_c();
@@ -31,16 +35,18 @@ impl JobTrait for SchedulerJob {
         //     })
         // }));
 
-        let job_async = Job::new_async(expression, |_uuid, _l| {
+        let job_async = Job::new_async(expr, |_uuid, _l| {
             Box::pin(async move {
                 match SystemTime::now().duration_since(UNIX_EPOCH) {
-                    Ok(n) => info!("MicroSec：{:?}", n.as_micros()),
-                    Err(_) => error!("System time error")
+                    Ok(n) => info!("ticker:{:?}ms", n.as_micros()),
+                    Err(_) => error!("system time error")
                 }
             })
         })?;
+
         sched.add(job_async).await?;
         sched.start().await?;
+
         Ok(())
     }
 }
