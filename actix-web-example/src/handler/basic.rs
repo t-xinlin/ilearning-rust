197716f896c9b7211f321d24da6b65d1e8b67bd7
t-xinlin <t_xinlin@sina.com>
2023-09-15 20:14:04 +0800

    Update basic.rs

--- actix-web-example/src/handler/basic.rs
+++ actix-web-example/src/handler/basic.rs
@@ -1,16 +1,49 @@
+use std::collections::HashMap;
+use std::sync::Mutex;
+
 use actix_web::{
     get,
     web,
     Responder,
     HttpRequest,
+    Error,
 };
-use std::sync::Mutex;
+
 // use actix_http::http::ContentEncoding;
+use actix_web_lab::respond::Html;
+use askama::Template;
+
+#[derive(Template)]
+#[template(path = "user.html")]
+struct UserTemplate<'a> {
+    name: &'a str,
+    text: &'a str,
+}
+
+#[derive(Template)]
+#[template(path = "index.html")]
+struct Index;
 
 #[get("/index.html")]
-pub async fn index(req: HttpRequest) -> impl Responder {
-    format!("Hello {} {}!", req.path().to_string(), "index2")
+async fn index(query: web::Query<HashMap<String, String>>) -> Result<impl Responder, Error> {
+    let html = if let Some(name) = query.get("name") {
+        UserTemplate {
+            name,
+            text: "Welcome!",
+        }
+            .render()
+            .expect("template should be valid")
+    } else {
+        Index.render().expect("template should be valid")
+    };
+
+    Ok(Html(html))
 }
+
+// #[get("/index.html")]
+// pub async fn index2(req: HttpRequest) -> impl Responder {
+//     format!("Hello {} {}!", req.path().to_string(), "index2")
+// }
 
 // // #[get("/test/{id}/{name}")]
 // async fn test(web::Path((id, name)): web::Path<(u32, String)>) -> impl Responder {
