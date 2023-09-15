46b1ccea7f8a4c6b6392ae04cdcb67706d16fe54
t-xinlin <t_xinlin@sina.com>
2023-09-15 20:10:28 +0800

    Update main.rs

--- actix-web-example/src/main.rs
+++ actix-web-example/src/main.rs
@@ -8,7 +8,9 @@ use actix_web::{
     App,
     HttpServer,
     http::header,
+    web,
 };
+
 use actix_web_example::{
     middleware,
     handler::{basic},
@@ -18,6 +20,7 @@ use actix_web_example::{
 pub mod utils;
 pub mod conf;
 pub mod model;
+pub mod handler;
 
 use crate::utils::{
     log as sys_log,
@@ -25,9 +28,11 @@ use crate::utils::{
     scheduler::JobTrait,
     counter::Iterator,
     counter,
+    tls,
     // signal,
 };
 use crate::conf::config;
+use crate::handler::StopHandle;
 
 #[tokio::main]
 async fn main() -> std::io::Result<()> {
@@ -54,25 +59,44 @@ async fn run() -> std::io::Result<()> {
     let conf = config::GLOBAL_CONFIG.lock().unwrap();
     info!("GLOBAL_CONFIG: {:?}",  conf.clone());
     let counter = basic::new_counter();
-    let mut app = HttpServer::new(move || App::new()
-        .wrap(Cors::default()
-                  .allow_any_origin() // .allowed_origin("*")
-                  .allowed_methods(vec!["GET", "POST", "DELETE"])
-                  .allowed_headers(vec![header::AUTHORIZATION, header::ACCEPT])
-                  .allowed_header(header::CONTENT_TYPE)
-                  .allowed_header("sign")
-                  .supports_credentials()
-                  .max_age(3600),
-        )
-        .wrap(middleware::AccessLogging::default().log_target("http_log"))
-        .app_data(counter.clone()) // <- register the created data
-        .configure(routes)
-    );
+    let stop_handle = web::Data::new(StopHandle::default());
+    let mut app = HttpServer::new({
+        let stop_handle = stop_handle.clone();
+        move || App::new()
+            .service(web::scope("/sys")
+                .app_data(stop_handle.clone())
+                .service(handler::stop)
+            )
+            .wrap(cors())
+            .wrap(middleware::AccessLogging::default().log_target("http_log"))
+            .app_data(counter.clone()) // <- register the created data
+            .configure(routes)
+    });
 
-    for i in conf.clone().server.services.unwrap() {
-        app = app.bind(format!("{}:{}", i.address.unwrap(), i.port.unwrap())).unwrap();
+    let config = tls::load_tls_config();
+    for s in conf.clone().server.services.unwrap() {
+        let addr = s.address.unwrap();
+        if String::from("127.0.0.1").eq(addr.as_str()) {
+            app = app.bind(format!("{}:{}", addr, s.port.unwrap())).unwrap();
+        } else {
+            app = app.bind_rustls(format!("{}:{}", addr, s.port.unwrap()), config.clone()).unwrap();
+        }
     }
-    app.run().await
+    app = app.workers(10);
+    let srv = app.run();
+    stop_handle.register(srv.handle());
+    srv.await
+}
+
+fn cors() -> Cors {
+    Cors::default()
+        .allow_any_origin() // .allowed_origin("*")
+        .allowed_methods(vec!["GET", "POST", "DELETE"])
+        .allowed_headers(vec![header::AUTHORIZATION, header::ACCEPT])
+        .allowed_header(header::CONTENT_TYPE)
+        .allowed_header("sign")
+        .supports_credentials()
+        .max_age(3600)
 }
 
 async fn scheduler_job() {
